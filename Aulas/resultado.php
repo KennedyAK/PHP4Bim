ab252ba902c41847921e06e6b512cbf62343f60f
Antonio Kennedy <antonioliveiraak@gmail.com>
2021-10-28 00:57:28 -0300

    adaptações em cima do conteudo da aula 04

--- Aulas/resultado.php
+++ Aulas/resultado.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultados</title>
+    <title>Resultado</title>
 </head>
 
 <body>
@@ -16,11 +16,11 @@
                 margin: 0 auto;
                 width: 600px;
             }
-            
+
             h3 {
                 text-align: center;
             }
-            
+
             table,
             th,
             td {
@@ -28,7 +28,7 @@
                 border-bottom: 1px solid black;
                 text-align: center;
             }
-            
+
             table {
                 border-bottom: 0px solid black;
             }
@@ -44,12 +44,36 @@
                     <th>Data</th>
                     <th>Status</th>
                 </tr>
-                <tr>
-                    <td>01</td>
-                    <td>Antonio</td>
-                    <td>2021-10-28 23:01</td>
-                    <td>Pendente</td>
-                </tr>
+                <?php
+                $conexao = new mysqli("localhost", "root", "", "bdphp");
+                if ($conexao->connect_error) {
+                    echo 'erro ao conectar ao BD' . $conexao->connect_error;
+                }
+                $resultado = $conexao->query("SELECT * FROM tarefa ORDER BY idtarefa");
+
+                $tupla = array();
+                while ($linha = $resultado->fetch_assoc()) {
+                    array_push($tupla, $linha);
+                }
+
+                foreach ($tupla as $conteudo) :
+                ?>
+                    <tr>
+                        <td>
+                            <?php echo ($conteudo['idtarefa']); ?>
+                        </td>
+                        <td>
+                            <?php echo ($conteudo['descricao']); ?>
+                        </td>
+                        <td>
+                            <?php echo ($conteudo['data']); ?>
+                        </td>
+                        <td>
+                            <?php echo ($conteudo['status']); ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+
             </table><br>
             <a href="exemplo03.php">Voltar</a>
         </fieldset>
